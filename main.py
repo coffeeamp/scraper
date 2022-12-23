fb331f21a539fc78b36b072071a4cf84fa34c7e3
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2022-12-23 21:02:41 +0900

    221223

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
 
+db = {} # cache
+
 @app.route("/") # start page
 def home():
     return render_template("home.html", name="suhyun") # home.html을 렌더링하고, name이라는 변수에 suhyun를 넣어줌
@@ -11,11 +13,15 @@ def home():
 @app.route("/search") # 검색창
 def search():
     keyword = request.args.get("keyword") # request.args.get("keyword")를 통해 검색창에 입력한 keyword를 가져옴
-    indeed = extract_indeed_jobs(keyword)
-    wwr = extract_wwr_jobs(keyword)
-    jobs = indeed + wwr # 두개의 리스트를 합침
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = indeed + wwr # 두개의 리스트를 합침
+        db[keyword] = jobs
     return render_template("search.html", keyword = keyword, jobs = jobs) # search.html을 렌더링하고, keyword와 jobs를 넣어줌
 
 
 
-app.run("127.0.0.1") # 서버를 실행시
+app.run("127.0.0.1") 
